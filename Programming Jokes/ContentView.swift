03d6906fbf306474efa7d2f01956aeff991e535f
Luke Xue <lukexue064@gmail.com>
2021-07-29 11:29:29 -0500

    Got API Key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -26,12 +26,8 @@ struct ContentView: View {
         })
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key707790e608msh5a8173a075dbd56p1b3540jsn8816ec66bacf"
+        
     }
 }
 
